aba44910485d376f074b696d27809e360962f684
jasperAnonymouse <xanthonyalvarezx@gmail.com>
2023-11-13 20:29:39 -0500

    parallax almost finished

---
+++ success-web-development/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import { React, useState } from "react";
+
+function Footer() {
+  return (
+    <>
+      <main>
+        <h1>Footer</h1>
+      </main>
+    </>
+  );
+}
+
+export default Footer;
