2dea04a97bd1c8f528bbcaecccdb282467f965b9
jasperAnonymouse <xanthonyalvarezx@gmail.com>
2023-11-19 17:43:06 -0500

    added footer content

--- success-web-development/src/components/Footer.jsx
+++ success-web-development/src/components/Footer.jsx
@@ -1,11 +1,45 @@
 import { React, useState } from "react";
+import "../styles/footer.scss";
 
 function Footer() {
   return (
     <>
-      <main>
-        <h1>Footer</h1>
-      </main>
+      <div id="footer-container">
+        <div className="footer-div">
+          <div id="footer-logo">
+          </div>
+        </div>
+        <div className="footer-div">
+          <ul className="footer-link-list">
+            <li className="footer-link">
+              Contact Us
+            </li>
+            <li className="footer-link">
+              Our Projects
+            </li>
+            <li className="footer-link">
+              Endorsements
+            </li>
+            <li className="footer-link">
+              Meet The Team
+            </li>
+            <li className="footer-link">
+              FAQ
+            </li>
+          </ul>
+        </div>
+        <div id="spacer-div" className="footer-div">
+        </div>
+        <div id="social-div" className="footer-div">
+          <div>
+            
+            <div id="follow-us-circle">
+              Follow Us!
+            </div>
+
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- success-web-development/src/components/pages/Landing.jsx
+++ success-web-development/src/components/pages/Landing.jsx
@@ -7,14 +7,21 @@ function Landing() {
       <main>
         <div className="parallax_wrapper">
           <div id="intro" className="parallax_group intro">
-            <h1>Philadelphia based web development company</h1>
+            <h1>We're a Philadelphia based web development company</h1>
           </div>
           
           <div id="group-1" className="parallax_group">
             <div className="parallax_layer base_layer">BASE</div>
             <div className="parallax_layer mid_layer">
               <div className="parallax_text">
-              We bring your every Idea to Life.  Wether your design is for a single page application or a large multi-page website. <br /> We do it all, from prersonal blogs, to enterprise work. <br /> We'll make sure your vision <br /> becomes your product <br />one-hundred percent of the time.
+                We bring your every Idea to Life.<br/>
+                Wether your design is for a single page application <br/>
+                or a large multi-page website. <br />
+                We do it all,<br/>
+                from prersonal blogs, to enterprise work. <br />
+                We'll make sure your vision, <br />
+                becomes your product. <br />
+                One-hundred percent of the time.
               </div>
             </div>
           </div>
@@ -22,7 +29,7 @@ function Landing() {
           <div id="group-2" className="parallax_group">
             <div className="parallax_layer mid_layer_2">
             <div className="parallax_text">
-              We're a team Of like-minded individuals, who thrive on producing top tier products to our clients.<br />Our combined experiences make us we'll rounded and capable of tackling any project.
+              We're a team Of like-minded individuals,<br/> who thrive on producing top tier products to our clients.<br />Our combined experiences make us we'll rounded and capable of tackling any project.
               </div>
             </div>
           </div>
